21845c248081fa3361740517b029e94f70572188
Jake Schaffer <jakecr8guru@gmail.com>
2020-05-19 16:19:32 -0500

    almost done

--- client.js
+++ client.js
@@ -41,3 +41,71 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+let bonus = 0;
+
+for (let i = 0; i < employees.length; i++) {
+  let employee = employees[i];
+  let peopleMoney = bonusNumberCal(employee);
+  // console.log(peopleMoney);
+}
+
+let test1 = bonusNumberCal(employees[0]);
+let test2 = bonusNumberCal(employees[1]);
+let test3 = bonusNumberCal(employees[2]);
+let test4 = bonusNumberCal(employees[3]);
+let test5 = bonusNumberCal(employees[4]);
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
+console.log(test5);
+
+function bonusNumberCal(employeeParam) {
+  console.log("In bonusNumberCal!");
+  let totalBonus = 0;
+  let moneyMoney = Number(employeeParam.annualSalary);
+
+  const retObj = {
+    name: employeeParam.name,
+    bonusPercentage: 0,
+    totalCompensation: 0,
+    totalBonus: 0
+  };
+  // employee.annualSalary +
+  // bonus = (employee.annualSalary * bonusRate
+  if(employeeParam.rating <= 2){
+  }
+  else if (employeeParam.rating === 3){
+    retObj.bonusPercentage = 0.04;
+  }
+  else if (employeeParam.rating === 4){
+    retObj.bonusPercentage = 0.06;
+  }
+  else if (employeeParam.rating === 5) {
+    retObj.bonusPercentage = 0.10;
+  }
+
+  if (employeeParam.employeeNumber.length === 4){
+    retObj.bonusPercentage = retObj.bonusPercentage + 0.05;
+  }
+  if(retObj.bonusPercentage > 0.13){
+    retObj.bonusPercentage = 0.13;
+  }
+
+  if (moneyMoney > 65000) {
+    retObj.bonusPercentage = retObj.bonusPercentage - 0.01;
+  }
+
+  // calculate that final bonus
+  // calculate that final compensation
+  retObj.totalCompensation = ((employeeParam.annualSalary * retObj.bonusPercentage)
+   + employeeParam.annualSalary)
+  return retObj;
+  // What do we want this to do?
+  // needs to return
+
+  //return object;
+
+}
+// 
